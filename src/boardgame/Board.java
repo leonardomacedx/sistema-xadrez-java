507081db793105180308d2f7c3ced4e4df953680
Leonardo Macedo <leo.depaula2019@gmail.com>
2023-03-10 11:32:19 -0300

    Camada xadrez e printando o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -3,13 +3,13 @@ package boardgame;
 public class Board {
 	
 	private int rows;
-	private int collumns;
+	private int columns;
 	private Piece[][] pieces;
 	
-	public Board(int rows, int collumns) {
+	public Board(int rows, int columns) {
 		this.rows = rows;
-		this.collumns = collumns;
-		pieces = new Piece[rows][collumns];
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
 	}
 
 	public int getRows() {
@@ -20,15 +20,20 @@ public class Board {
 		this.rows = rows;
 	}
 
-	public int getCollumns() {
-		return collumns;
+	public int getColumns() {
+		return columns;
 	}
 
-	public void setCollumns(int collumns) {
-		this.collumns = collumns;
+	public void setColumns(int columns) {
+		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
 	
-	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 	
 }
